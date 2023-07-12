237d29566f91309438fdc854d3872ad52b208ee8
Hr-jihan <127619656+Hr-jihan@users.noreply.github.com>
2023-07-12 12:51:01 +0600

    Add files via upload

---
+++ js/aytor.js
@@ -0,0 +1,12 @@
+
+// $('.menu').click (function(){
+//     $(this).toggleClass('is-active ');
+//   });
+
+$('#banner').slick({
+    arrows:false,
+    dots:true,
+    dotsClass:"slick-dots container",
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
